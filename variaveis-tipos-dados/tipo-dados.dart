e551d80945f772705155a6a1bd95393b32dcb4f9
Jean Carlos <jeancc.costa@gmail.com>
2022-04-30 21:40:15 -0300

    Atualização

---
+++ variaveis-tipos-dados/tipo-dados.dart
@@ -0,0 +1,7 @@
+void main() {
+  String nome = "Ariel";
+  int idade = 28;
+  double salario = 1500.00;
+  bool casado = true;
+  print(salario);
+}
---
+++ variaveis-tipos-dados/var_final_const.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+void main() {
+  print("Escreva o nome do curso");
+  var curso = stdin.readLineSync();
+  print("o nome do curso é $curso");
+}
